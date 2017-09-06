48c8cda3e1585bb002bf295ea21884e1c4e983a0
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-09-06 11:37:11 -0700

    tuesday white boarding

--- index.js
+++ index.js
@@ -14,44 +14,43 @@ function connectSiblings(root) {
   let level = 0;
   let current = root;
   queue.push({
-    level: current.level + 1,
-    node: current.left
+    level: 1,
+    node: current
   });
 
   while (queue.length) {
     current = queue.shift();
-
-    if (queue[0] !== undefined) {
-      console.log('what is current.sibling', current.sibling);
-      console.log('what is queue', queue[0]);
+    console.log('crrent.level==', current.level);
+    console.log('queuelevel==', queue);
+    if(queue[0]){
       if (current.level === queue[0].level) {
-
         current.sibling = queue[0];
       }
-
-      if (current.left) {
-        queue.push({
-          level: current.level + 1,
-          node: current.left
-        });
-      }
-      if (current.right) {
-        queue.push({
-          level: current.level + 1,
-          node: current.right
-        });
-      }
     }
+
+    if (current.left) {
+      queue.push({
+        level: current.level + 1,
+        node: current.left
+      });
+    }
+    if (current.right) {
+      queue.push({
+        level: current.level + 1,
+        node: current.right
+      });
+    }
+
   }
+  return current;
 }
 
 let lvl2d = new Node(7, null, null, null);
 let lvl2c = new Node(6, null, null, null);
 let lvl2b = new Node(5, null, null, null);
 let lvl2a = new Node(4, null, null, null);
-let rR = new Node(3, lvl2c, lvl2d, null);
-let rL = new Node(2, lvl2a, lvl2b, null);
-let rootNode = new Node(1, rL, rR, null);
+let right = new Node(3, lvl2c, lvl2d, null);
+let left = new Node(2, lvl2a, lvl2b, null);
+let rootNode = new Node(1, left, right, null);
 let tree = new BST(rootNode);
-connectSiblings(tree);
-console.log('tree=', tree);
+console.log(connectSiblings(tree));
---
+++ index1.js
@@ -0,0 +1,53 @@
+function Node(val, left, right, next) {
+  this.val = val;
+  this.left = left;
+  this.right = right;
+  this.next = next;
+}
+
+let connect = function(root) {
+  let queue = [];
+  queue.push(root);
+  bfs();
+
+  function bfs(){
+    let tempQueue = [];
+    let prevNode = null;
+    while(queue.length > 0){
+      let current = queue.shift();
+      if(current.left){
+        tempQueue.push(current.left);
+        console.log('what is current.left', current.left);
+        if(prevNode){
+          prevNode.next = current.left;
+          // console.log('prevNode ===', prevNode);
+        }
+        prevNode = current.left;
+      }
+
+      if(current.right){
+        tempQueue.push(current.right);
+        if(prevNode){
+          prevNode.next = current.right;
+          // console.log('prevNode ===', prevNode);
+        }
+        prevNode = current.right;
+      }
+    }
+    // console.log('wat is tempQueue', tempQueue);
+    queue = tempQueue;
+    bfs();
+    return root;
+  }
+};
+
+//test case
+let lvl2d = new Node(7, null, null, null);
+let lvl2c = new Node(6, null, null, null);
+let lvl2b = new Node(5, null, null, null);
+let lvl2a = new Node(4, null, null, null);
+let right = new Node(3, lvl2c, lvl2d, null);
+let left = new Node(2, lvl2a, lvl2b, null);
+let rootNode = new Node(1, left, right, null);
+
+console.log(connect(rootNode));
